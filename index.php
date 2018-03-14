ddfe9658112a73e4aaa10c4e9e993236b03a10f5
aasf0543a <33273330+aasf0543a@users.noreply.github.com>
2018-03-14 17:06:59 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,15 @@
-<?php include_once("line-notify.html"); ?>
+<!doctype html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+<h1>LINE Notifyサンプル</h1>
+
+<ul>
+    <li><a href="start_authorize.php">認証開始</a></li>
+    <li><a href="callme.php">メッセージ送信</a></li>
+</ul>
+</body>
+</html>
