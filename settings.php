acd89d97959ee274c95bdb132c613a35762a49dc
aasf0543a <33273330+aasf0543a@users.noreply.github.com>
2018-03-14 17:16:58 +0800

    Update settings.php

--- settings.php
+++ settings.php
@@ -1,6 +1,6 @@
 <?php
 // ratelimitのリセット時刻のタイムゾーンを指定
 date_default_timezone_set('Asia/Taipei');
-define('LINE_NOTIFY_CLIENT_ID', '	uDxoIBod0LePU0UHI82MqR');
+define('LINE_NOTIFY_CLIENT_ID', 'uDxoIBod0LePU0UHI82MqR');
 define('LINE_NOTIFY_CLIENT_SECRET', '9meXMWULRIzVa7TWYJbG2FQHEHXlqQnxNxEW0Rcx06E');
 define('CALLBACK_URL', 'https://lineage0314.herokuapp.com/callback.php');
